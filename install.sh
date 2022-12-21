371eb9199972a44572d9030b66d853682a82a3ae
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2022-12-21 15:07:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,6 @@ rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 yum install php56w php56w-pdo php56w-mysql php56w-mbstring php56w-pear php56w-process php56w-xml php56w-opcache php56w-ldap php56w-intl php56w-soap -y
 sed -i 's/\(^upload_max_filesize = \).*/\120M/' /etc/php.ini
 cd /var/www
-cp contrib/httpd/asterisk-cdr-viewer.conf /etc/httpd/conf.d/
+cp contrib/httpd/asterisk_cdr_viewer.conf /etc/httpd/conf.d/
 systemctl restart httpd.service
 systemctl status httpd.service
